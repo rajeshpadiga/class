0c9e10d6b5a93977a95375edc8fe4b26ce487616
Rajesh <padigarajesh219@gmail.com>
2021-11-10 22:55:11 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace GitDemo5
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+       static void Main()
+    {
+        int[] Numbers = new int[3];
+        Numbers[0] = 1001;
+        Numbers[1] = 1002;
+        Numbers[2] = 1003;
+
+        //ParamsMethod(Numbers);
+        ParamsMethod(10, 20, 30, 50);
     }
+
+    static void ParamsMethod(params int[] numbers)
+    {
+        Console.WriteLine("Number of elements {0}", numbers.Length);
+
+        foreach (int n in numbers)
+            Console.WriteLine(n);
+    }
+}
 }
